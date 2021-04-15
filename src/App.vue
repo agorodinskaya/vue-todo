9f3092776b894425c2a8df3d2cd096a041620c42
Anastasia Gorodinskaya <agorodinskaya@gmail.com>
2021-04-16 08:55:17 +1000

    add delete funcitonality

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="container">
   <Header title="task tracker"/>
-  <Tasks :tasks='tasks'/>
+  <Tasks @delete-task="deleteTask" :tasks='tasks'/>
   </div>
 </template>
 
@@ -18,6 +18,13 @@ export default {
   data() {
     return {
       tasks : []
+    }
+  },
+  methods: {
+    deleteTask(id) {
+      if(confirm('Please confirm you want to delete')) {
+        this.tasks = this.tasks.filter(task => task.id !== id)
+      }
     }
   },
   created() {
